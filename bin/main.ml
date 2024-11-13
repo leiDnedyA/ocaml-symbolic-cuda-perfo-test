da9de9e1d1390358d28f893a0cc79b6af5bc56da
ayden <aydendiel@gmail.com>
2024-11-13 13:13:48 -0500

    refactor: got rid of semicolons

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,7 @@
 module IntSet = Set.Make(struct
   type t = int
   let compare = compare
-end);;
+end)
 
 let count_accesses (enabled_array: bool array) (warp_count: int) (word_size: int): int = 
   let accesses = ref IntSet.empty in
@@ -9,10 +9,10 @@ let count_accesses (enabled_array: bool array) (warp_count: int) (word_size: int
     if enabled_array.(i - 1) == true then 
       accesses := IntSet.add ((i - 1) / word_size) !accesses;
   done;
-  IntSet.cardinal !accesses;;
+  IntSet.cardinal !accesses
 
-let enabled = [|true; false; true; true; true|];;
-let warp_count = Array.length enabled;;
-let word = 4;;
+let enabled = [|true; false; true; true; true|]
+let warp_count = Array.length enabled
+let word = 4
 
 Printf.printf "%d\n" (count_accesses enabled warp_count word) 
