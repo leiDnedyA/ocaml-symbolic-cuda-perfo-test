b6e40f6c4b97f7f7c4cd7d2c02b4d5968424cbbf
ayden <aydendiel@gmail.com>
2024-11-20 14:20:31 -0500

    reafactor: implemented test type

--- bin/main.ml
+++ bin/main.ml
@@ -1,52 +1,72 @@
-(* module IntSet = Set.Make(struct *)
-(*   type t = int *)
-(*   let compare = compare *)
-(* end) *)
-(**)
-(* let count_accesses (enabled_array: bool array) (warp_count: int) (word_size: int): int =  *)
-(*   let accesses = ref IntSet.empty in *)
-(*   for i = 1 to warp_count do *)
-(*     if enabled_array.(i - 1) == true then  *)
-(*       accesses := IntSet.add ((i - 1) / word_size) !accesses; *)
-(*   done; *)
-(*   IntSet.cardinal !accesses *)
-(**)
-
 open Z3
 
-(* Example usage with Z3 *)
-let () =
+type t = {
+  name: string;
+  expressions: Expr.expr list;
+  expected_count: int;
+}
 
-  (* let enabled = [|true; false; true; true; true|] *)
-  (* let warp_count = Array.length enabled *)
-  (* let word = 4 *)
-  (* *)
-  (* Printf.printf "%d\n" (count_accesses enabled warp_count word)  *)
-
-
-  (* Create Z3 context *)
+(* Helper function to create Z3 context and test different inputs *)
+let test_inputs () =
   let ctx = mk_context [] in
-
   let bin f x y = f ctx [x;y] in 
-  let listless_bin f x y = f ctx x y in
-
-  (* Define some partially-defined helper functions to simplify z3 syntax *)
   let add = bin Arithmetic.mk_add in
-  let div = listless_bin Arithmetic.mk_div in
+  let div = Arithmetic.mk_div ctx in
+  let mul = bin Arithmetic.mk_mul in
+  let num = Arithmetic.Integer.mk_numeral_i ctx in
+  let const ch = Arithmetic.Integer.mk_const ctx (Symbol.mk_string ctx ch) in
+  
+  (* Create base variable *)
+  let a = const "a" in
+  let numeral_4 = num 4 in
 
-  let a = Arithmetic.Integer.mk_const ctx (Symbol.mk_string ctx "a")  in
+  let div_list_by_4 = List.map (fun x -> div x numeral_4) in
+  
+  (* Test Case 1: Original sequence [a, a+1, a+2, a+3] divided by 4 *)
+  let test1 = {
+    name = "Original";
+    expressions = div_list_by_4 [
+      a;
+      add a (num 1);
+      add a (num 2);
+      add a (num 3);];
+    expected_count = 1;
+  } in
 
-  (* Create expressions for `expression_list = [a, a + 1, a + 2, a + 3]` pointwise divided by 4 *)
-  let numeral_4 = Arithmetic.Integer.mk_numeral_i ctx 4 in
-  let expression_list = List.map (fun x -> div x numeral_4) [
-    a;
-    add a (Arithmetic.Integer.mk_numeral_i ctx 1);
-    add a (Arithmetic.Integer.mk_numeral_i ctx 2);
-    add a (Arithmetic.Integer.mk_numeral_i ctx 3);
-  ] in
+  (* Test Case 2: Sequence designed to maximize overlaps: [4a, 4a, 4a, 4a] divided by 4 *)
+  let test2 = {
+    name = "All Same";
+    expressions = div_list_by_4 [
+      mul a numeral_4;
+      mul a numeral_4;
+      mul a numeral_4;
+      mul a numeral_4;
+    ];
+    expected_count = 1;
+  } in
+  
+  (* Test Case 3: Another attempt with [4a, 4a+4, 4a+8, 4a+12] divided by 4 *)
+  let test3 = {
+    name = "Multiples of 4";
+    expressions = div_list_by_4 [
+      mul a numeral_4;
+      add (mul a numeral_4) (num 4);
+      add (mul a numeral_4) (num 8);
+      add (mul a numeral_4) (num 12);
+    ];
+    expected_count = 4;
+  } in
+  
 
-  (* Call the count_symbolic_accesses function *)
-  let (unique_count, final_constraints) = Overlap_count.count_symbolic_accesses ctx expression_list in
-  (* Print result *)
-  Printf.printf "Unique overlapping expressions: %d\n" unique_count;
-  Printf.printf "Final constraints: %s\n" (Expr.to_string final_constraints);
+  (* Run tests *)
+  List.iter (function {name; expressions; expected_count} ->
+    let (count, constraints) = Overlap_count.count_symbolic_accesses ctx expressions in
+    Printf.printf "\nTest %s:\n" name;
+    Printf.printf "Unique overlapping expressions: %d\n" count;
+    Printf.printf "Expected count: %d\n" expected_count;
+    Printf.printf "Final constraints: %s\n" (Expr.to_string constraints)
+  ) [test1; test2; test3;]
+;;
+
+(* Run the tests *)
+let () = test_inputs ()
